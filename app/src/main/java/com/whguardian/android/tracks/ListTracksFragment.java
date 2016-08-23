c2af100d710ade9a762efa604d42391ffa10d132
whguardian <whguardian@gmail.com>
2016-08-24 00:41:29 +0800

    ashdga

---
+++ app/src/main/java/com/whguardian/android/tracks/ListTracksFragment.java
@@ -0,0 +1,20 @@
+package com.whguardian.android.tracks;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by whguardian on 2016/8/23.
+ */
+public class ListTracksFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        return null;
+    }
+}
+
+
